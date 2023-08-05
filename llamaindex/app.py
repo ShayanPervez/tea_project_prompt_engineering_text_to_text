410df563618eb2af4b9054b4fd5254edb6873426
Shayan Pervez <54498354+ShayanPervez@users.noreply.github.com>
2023-08-06 00:51:33 +0530

    Update app.py

--- llamaindex/app.py
+++ llamaindex/app.py
@@ -1,7 +1,13 @@
-import json
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.node_parser import SimpleNodeParser
 import os
-
-from importing import *
+from llama_index import StorageContext, load_index_from_storage
+from llama_index import Document
+import openai
+from llama_index.evaluation import DatasetGenerator
+import json
+from datetime import datetime
+import streamlit as st
 
 openai.api_key = "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"
 
